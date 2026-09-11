85cd934805ce92302987bf03b2524161365fd877
Jaival <jaivalsuthar21@gmail.com>
2026-09-11 15:32:10 +0530

    feat: add markdown-aware chunking

--- src/knowledge_hub/chunking/__init__.py
+++ src/knowledge_hub/chunking/__init__.py
@@ -1,5 +1,6 @@
 from .base import Chunker
+from .markdown import MarkdownChunker
 from .pdf import PdfChunker
 from .recursive import RecursiveChunker
 
-__all__ = ["Chunker", "PdfChunker", "RecursiveChunker"]
+__all__ = ["Chunker", "MarkdownChunker", "PdfChunker", "RecursiveChunker"]
