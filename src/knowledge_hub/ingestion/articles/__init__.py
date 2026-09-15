9307deb4322e62eeef09f78d45e6bdae64a5e0cb
Jaival <jaivalsuthar21@gmail.com>
2026-09-15 15:37:54 +0530

    feat: add article ingestion contracts

---
+++ src/knowledge_hub/ingestion/articles/__init__.py
@@ -0,0 +1,5 @@
+"""Contracts for source-neutral article ingestion."""
+
+from .contracts import Article, ArticleBlock, ArticleBlockType, ArticleSection
+
+__all__ = ["Article", "ArticleBlock", "ArticleBlockType", "ArticleSection"]
